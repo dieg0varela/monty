3e1ff2ef5f6f75bc281d287a531e9468e6fe5237
toshi-uy <s_borgia@hotmail.com>
2021-05-13 13:16:49 +0000

    changed condition to 32

--- pchar.c
+++ pchar.c
@@ -21,7 +21,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 	temp = *stack;
 	while (temp->next != NULL)
 		temp = temp->next;
-	if (temp->n < 32 && temp->n > 127)
+	if (temp->n < 32 && temp->n > 126)
 	{
 		fprintf(stderr, "L%i: can't pchar, value out of range\n", line_number);
 		free_dlistint(monty.stack);
