f3a1d948c988582f30654f7d7140350bc7dbfda7
toshi-uy <s_borgia@hotmail.com>
2021-05-13 01:16:47 +0000

    added nop func

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - removes the top element of the stack
+ * @stack: pointer to stack
+ * @line_number: line number
+ *
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
