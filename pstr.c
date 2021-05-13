580de46a57becd6edfe8561f5fec9dde613aca7e
toshi-uy <s_borgia@hotmail.com>
2021-05-13 13:07:20 +0000

    added if \0 condition

---
+++ pstr.c
@@ -0,0 +1,38 @@
+#include "monty.h"
+
+/**
+ * pstr - prints the char at the top of the stack, followed by a new line.
+ * @stack: pointer to stack
+ * @line_number: line number
+ *
+ */
+
+void pstr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = NULL;
+
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%i: can't pchar, stack empty\n", line_number);
+		free_dlistint(monty.stack);
+		fclose(monty.fp);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+        while (temp)
+	{
+		if (temp->n < 0 && temp->n > 127)
+		{
+			fprintf(stderr, "L%i: can't pchar, value out of range\n", line_number);
+			free_dlistint(monty.stack);
+			fclose(monty.fp);
+			exit(EXIT_FAILURE);
+		}
+		if (temp->n == '\0')
+			break;
+		printf("%c\n", temp->n);
+		temp = temp->prev;
+	}
+}
